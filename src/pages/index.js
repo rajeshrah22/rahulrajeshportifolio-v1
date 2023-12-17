5ec4dca8e52fc7ecb095529f1418e4022dfb2195
rajeshrah22 <rajeshrah22@gmail.com>
2023-12-17 18:07:11 -0500

    removed icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
 				<title>Rahul's Portfolio</title>
 				<meta name="description" content="Joel's Portfolio" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.jpeg" />
+				{/* <link rel="icon" href="/favicon.jpeg" /> */}
 			</Head>
 			<div>
 				<Hero />

965ae69bfee291ce4182e2db206b8843d9549fa5
Rahul Rajesh <109564679+rajeshrah22@users.noreply.github.com>
2024-04-30 13:46:04 -0400

    Update navbar.jsx
    Removed resume button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
                     Rahul's Portfolio
                 </Link>
             </div>
-            <a href="https://docs.google.com/document/d/1n6vH1zZVeiBm6dhq7gweRAU2LBofoHUp/edit?usp=sharing&ouid=103967494602058031693&rtpof=true&sd=true" target="_blank" className="cta-btn">Resume</a>
         </div>
     )
 }
